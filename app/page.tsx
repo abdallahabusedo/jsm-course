efa810f5cd0e98a89a3b6892bee631dffc805cf0
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2025-01-12 17:13:55 +0200

    enhance styling and theming with new utility classes and color palette

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   console.log("What is the Image component?");
 
   return (
-    <h1 className="text-3xl font-bold text-white">Welcome to Next Js 15</h1>
+    <h1 className="h1-bold background-light900_dark300 text-dark100_light900">
+      Welcome to Next Js 15
+    </h1>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,51 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          100: "#fff1e6",
+          500: "#ff7000",
+        },
+        dark: {
+          100: "#000000",
+          200: "#0f1117",
+          300: "#151821",
+          400: "#212734",
+          500: "#3f4354",
+        },
+        light: {
+          900: "#ffffff",
+          850: "#fdfdfd",
+          800: "#f4f6f8",
+          700: "#dce3f1",
+          500: "#7bbec8",
+          400: "#858ead",
+        },
+      },
+      borderRadius: {
+        "2": "8px",
+        "1.5": "6px",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      boxShadow: {
+        "light-100":
+          "0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px rgba(184, 184, 184, 0.02), 0px 2px 4px 0px rgba(184, 184, 184, 0.03)",
+        "light-200": "10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+        "light-300": "-10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+        "dark-100": "box-shadow: 0px 4px 4px 0px #00000040",
+        "dark-200": "2px 0px 20px 0px rgba(39, 36, 36, 0.04)",
+      },
+      backgroundImage: {
+        "auth-dark": 'url("/images/auth-dark.png")',
+        "auth-light": 'url("/images/auth-light.png")',
+      },
+      screens: {
+        xs: "420px",
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        "space-grotesk": ["var(--font-space-grotesk)"],
       },
     },
   },
