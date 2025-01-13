e67a11f1a3d52f4daebe0c9d7d4805e9097c6d6c
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2025-01-13 15:24:43 +0200

    add new SVG icons for navigation and UI elements

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ export default function Home() {
   console.log("What is the Image component?");
 
   return (
-    <h1 className="h1-bold background-light900_dark300 text-dark100_light900">
-      Welcome to Next Js 15
-    </h1>
+    <>
+      <h1 className="h1-bold background-light900_dark300 text-dark100_light900">
+        Welcome to Next Js 15
+      </h1>
+      <h1 className="h1-bold background-light900_dark300 text-dark100_light900 font-space-grotesk">
+        Welcome to Next Js 15
+      </h1>
+    </>
   );
 }
