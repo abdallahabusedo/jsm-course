d548a5931fd5ed7a0f3df2f666cb3eab3a3ba535
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2025-01-13 23:16:18 +0200

    Mobile nav and nav links

---
+++ components/navigation/navbar/NavLink.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { SheetClose } from "@/components/ui/sheet";
+import { SIDEBAR_LINKS } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const NavLink = ({ isMobileNav = false }: { isMobileNav?: boolean }) => {
+  const pathname = usePathname();
+  const userId = "12345";
+  return (
+    <>
+      {SIDEBAR_LINKS.map((link, index) => {
+        const isActive =
+          (pathname.includes(link.route) && link.route.length > 1) ||
+          pathname === link.route;
+
+        if (link.route === "/profile") {
+          if (userId) link.route = `/profile/${userId}`;
+          else return null;
+        }
+
+        const LinkComponent = (
+          <Link
+            className={cn(
+              isActive
+                ? "primary-gradient rounded-lg text-light-900"
+                : "text-dark300_light900",
+              "flex items-center gap-4 bg-transparent p-4"
+            )}
+            href={link.route}
+            key={link.label}
+          >
+            <Image
+              src={link.imgURL}
+              alt={link.label}
+              width={20}
+              height={20}
+              className={cn({ "invert-color": !isActive })}
+            />
+            <p
+              className={cn(
+                isActive ? "base-bold" : "base-medium",
+                !isMobileNav && "max-lg:hidden"
+              )}
+            >
+              {link.label}
+            </p>
+          </Link>
+        );
+
+        return isMobileNav ? (
+          <SheetClose asChild>{LinkComponent}</SheetClose>
+        ) : (
+          <React.Fragment key={link.route}>{LinkComponent}</React.Fragment>
+        );
+      })}
+    </>
+  );
+};
+
+export default NavLink;
