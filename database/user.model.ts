18544f8d2009044baaaee573f11fc04523176d1a
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2025-01-17 17:06:52 +0200

    create user model

---
+++ database/user.model.ts
@@ -0,0 +1,32 @@
+import { model, models, Schema } from "mongoose";
+
+export interface IUser {
+  name: string;
+  email: string;
+  username: string;
+  bio?: string;
+  image: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+}
+
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
+    bio: { type: String, required: false },
+    image: { type: String, required: true },
+    location: { type: String, required: false },
+    portfolio: { type: String, required: false },
+    reputation: { type: Number, required: false, default: 0 },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = models?.user || model<IUser>("User", UserSchema);
+
+export default User;
