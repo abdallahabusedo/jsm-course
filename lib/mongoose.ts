22c149ce61cd28e028a3c9cbc1490731edbc739f
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2025-01-17 21:56:10 +0200

    add logging with pino and integrate into MongoDB connection and error handling

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,4 +1,5 @@
 import mongoose, { Mongoose } from "mongoose";
+import logger from "./logger";
 
 const MONGOOSE_URI = process.env.MONGODB_URI as string;
 
@@ -25,6 +26,7 @@ if (!cached) {
 
 const dbConnect = async (): Promise<Mongoose> => {
   if (cached.conn) {
+    logger.info("Using existing MongoDB connection");
     return cached.conn;
   }
 
@@ -34,11 +36,11 @@ const dbConnect = async (): Promise<Mongoose> => {
         dbName: "devflow",
       })
       .then((result) => {
-        console.log("Connected to MongoDB");
+        logger.info("Connected to MongoDB");
         return result;
       })
       .catch((error) => {
-        console.error("Error connecting to MongoDB", error);
+        logger.error("Error connecting to MongoDB", error);
         return error;
       });
   }
