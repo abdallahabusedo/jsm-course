fe0bf61d51e67134a4c7721795f9dd1e9b8fbd32
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2025-01-17 23:29:20 +0200

    add ticket management API with CRUD operations and search functionality

---
+++ app/database/database.ts
@@ -0,0 +1,100 @@
+const tickets = [
+  {
+    id: 1,
+    name: "fixed next js api route bug ",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 2,
+    name: "add new feature to the app",
+    status: "open",
+    type: "feature",
+  },
+  {
+    id: 3,
+    name: "fix the bug in the user profile",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 4,
+    name: "add new feature to the app",
+    status: "open",
+    type: "feature",
+  },
+  {
+    id: 5,
+    name: "fix the bug in the user profile",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 6,
+    name: "add new feature to the app",
+    status: "open",
+    type: "feature",
+  },
+  {
+    id: 7,
+    name: "fix the bug in the user profile",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 8,
+    name: "add new feature to the app",
+    status: "open",
+    type: "feature",
+  },
+  {
+    id: 9,
+    name: "fix the bug in the user profile",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 10,
+    name: "add new feature to the app",
+    status: "open",
+    type: "feature",
+  },
+  {
+    id: 11,
+    name: "fix the bug in the user profile",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 12,
+    name: "add new feature to the app",
+    status: "open",
+    type: "feature",
+  },
+  {
+    id: 13,
+    name: "fix the bug in the user profile",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 14,
+    name: "add new feature to the app",
+    status: "open",
+    type: "feature",
+  },
+  {
+    id: 15,
+    name: "fix the bug in the user profile",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 16,
+    name: "add new feature to the app",
+    status: "open",
+    type: "feature",
+  },
+];
+
+export default tickets;
